82a64269974261bf3bbf2412f3ac22a6b0e60e74
Bike Taxi APP <jothisbusiness@gmail.com>
2025-02-07 21:09:07 +0530

    Added full project (frontend + backend)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AppRegistry } from 'react-native';
+import App from './App'; // Ensure this path is correct
+import { name as appName } from './app.json';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+AppRegistry.registerComponent(appName, () => App);
